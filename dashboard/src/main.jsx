4fccb22794466c8b9f620cb7e3941d1cee14cb19
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-11-04 18:30:12 +0530

    Update main.jsx

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 export const Context = createContext({ isAuthenticated: false });
+
 const AppWrapper = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [admin, setAdmin] = useState({});
