2914b014f684fbb86c74c5a75d5365a6c705913d
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-12-23 21:54:04 +0530

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -7,7 +7,6 @@ import {
   Navigate,
 } from "react-router-dom";
 
-
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import AddNewDoctor from "./components/AddNewDoctor";
