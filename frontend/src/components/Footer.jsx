db6e33da8eaae652901d6e1f8f5af4108e4daa26
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-12-26 00:27:10 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { FaLocationArrow, FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
-
 const Footer = () => {
   const hours = [
     {
