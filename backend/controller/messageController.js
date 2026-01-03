c098c735b6e93100cfa5d015514017544d92ef7e
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-01-03 21:10:10 +0530

    Remove unnecessary blank lines in messageController

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -5,7 +5,6 @@ import { Message } from "../models/messageSchema.js";
 
 
 
-
 export const sendMessage = catchAsyncErrors(async (req, res, next) => {
   const { firstName, lastName, email, phone, message } = req.body;
   if (!firstName || !lastName || !email || !phone || !message) {
