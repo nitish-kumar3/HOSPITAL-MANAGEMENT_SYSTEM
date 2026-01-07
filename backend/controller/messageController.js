39a0deeee502f7e13e7536876dffec26905e8e40
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-01-07 22:53:48 +0530

    Add empty line for better code readability

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -3,6 +3,8 @@ import ErrorHandler from "../middlewares/error.js";
 import { Message } from "../models/messageSchema.js";
 
 
+   
+
 export const sendMessage = catchAsyncErrors(async (req, res, next) => {
   const { firstName, lastName, email, phone, message } = req.body;
   if (!firstName || !lastName || !email || !phone || !message) {
