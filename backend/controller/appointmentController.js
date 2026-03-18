0023cef60c028be7b0fcb4cc5534df918e9ce243
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-03-19 00:04:41 +0530

    Update appointmentController.js

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -3,7 +3,6 @@ import ErrorHandler from "../middlewares/error.js";
 import { Appointment } from "../models/appointmentSchema.js";
 import { User } from "../models/userSchema.js";
 
-
 export const postAppointment = catchAsyncErrors(async (req, res, next) => {
   const {
     firstName,
